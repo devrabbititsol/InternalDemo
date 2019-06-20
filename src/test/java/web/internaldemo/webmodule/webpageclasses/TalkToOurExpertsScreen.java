ad1029055cccf00436eec1e2824e88851489e798
root <santhosh.annam@devrabbit.com>
2019-06-20 10:38:14 +0530

    Test

--- src/test/java/web/internaldemo/webmodule/webpageclasses/TalkToOurExpertsScreen.java
+++ src/test/java/web/internaldemo/webmodule/webpageclasses/TalkToOurExpertsScreen.java
@@ -9,7 +9,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 @SuppressWarnings("unused")
 public class TalkToOurExpertsScreen extends BaseClass {
 	
-	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"InternalDemo\",\"project_description\":\"desc\",\"project_id\":299,\"module_name\":\"WebModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":435,\"testcase_name\":\"TC_Devrabbit_Contact\",\"testcase_id\":484,\"testset_id\":0,\"executed_timestamp\":1869990625,\"browser_type\":\"chrome\"}";
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbit.com/\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"InternalDemo\",\"project_description\":\"desc\",\"project_id\":299,\"module_name\":\"WebModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":435,\"testcase_name\":\"TC_Devrabbit_Contact\",\"testcase_id\":484,\"testset_id\":0,\"executed_timestamp\":1934162303,\"browser_type\":\"chrome\"}";
 
 	public static String projectName = "internaldemo";
 	public WebDriver driver;
